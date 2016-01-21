67de898dd0d3e35ca5fee53c809af11f3c088bfc
jebrogden <jebrogden@gmail.com>
2016-01-21 16:46:22 -0700

    Numbers and Math

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# This prints out the string
+print "I will now count my chickens:"
+
+# Print out hens/roosters and then solves the equations
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+# Prints out the string
+print "Now I will count the eggs:"
+
+# Prints out the equation with answer
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# This prints out the string
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# This prints out the actual solution
+print 3 + 2 < 5 - 7
+
+# These print the string and then display the solutions
+print "What is 3 + 2?", 3 + 2
+print "what is 5 - 7?", 5 - 7
+
+# This prints out the string
+print "Oh, that's why it's False."
+
+# This prints out the string
+print "How about some more."
+
+# These print out a string and then display the solution
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
